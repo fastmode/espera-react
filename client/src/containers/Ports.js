867108f137a25c4cef42f41d0c3072f3b3f18cb1
Art Rodriguez <art.rodriguez@live.com>
2018-02-08 16:50:10 -0500

    Setup fetch for Rails API

--- client/src/containers/Ports.js
+++ client/src/containers/Ports.js
@@ -5,10 +5,10 @@ const Ports = (props) => (
     <h1>ESPERA | Ports</h1>
     {props.ports.map(port => 
       <div className="PortCard">
-        <h3>{port.name}</h3>
-        <p>Country: {port.country}</p>
-        <p>Status: {port.status}</p>
-        <p>Delay: {port.delay} mins</p>
+        <h3>{port.port_name}</h3>
+        <p>{port.border}</p>
+        <p>Crossing Name: {port.crossing_name}</p>
+        <p>Hours: {port.hours}</p>
       </div>
     )}
   </div>
