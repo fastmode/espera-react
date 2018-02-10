718d22374947751cb3e2e99688cc227535e4846b
Art Rodriguez <art.rodriguez@live.com>
2018-02-10 08:31:37 -0500

    add FavoritePorts container

---
+++ client/src/containers/FavoritePorts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PortCard from '../components/PortCard';
+
+
+class FavoritePorts extends Component {
+
+
+
+
+  
+}
