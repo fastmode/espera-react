44cacfc704c7f2ba0db99939d4536c1e0d19d63f
Art Rodriguez <art.rodriguez@live.com>
2018-02-10 08:05:32 -0500

    make App.js into a component

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Ports from '../containers/Ports';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Ports />
+  </div>
+)
+
+export default App;
