4fe63d40a6ee923555cbdea529388e60717f4c75
Art Rodriguez <art.rodriguez@live.com>
2018-02-11 16:39:42 -0500

    Filtered Canada and Mexico ports

--- client/src/containers/Ports.js
+++ client/src/containers/Ports.js
@@ -5,15 +5,37 @@ import { getPorts } from '../actions/ports';
 
 class Ports extends Component {
 
+  state = {
+    favoritePorts: [],
+  }
+
   componentDidMount() {
     this.props.getPorts()
   }
 
   render() {
+
+    const canada = this.props.ports.filter(port => port.border === "Canadian Border");
+    const mexico = this.props.ports.filter(port => port.border === "Mexican Border");
+
     return (
       <div>
-        <h1>ESPERA | Ports</h1>
-        {this.props.ports.map(port => <PortCard key={port.id} port={ port } />)}
+        <div>
+          <h1>Canadian Ports</h1>
+          {canada.map(port => 
+            <PortCard 
+              key={port.id} 
+              port={ port } />
+          )}
+        </div>
+        <div>
+          <h1>Mexican Ports</h1>
+          {mexico.map(port =>
+            <PortCard
+              key={port.id}
+              port={ port } />
+          )}
+        </div>
       </div>
     );
   }
@@ -26,3 +48,23 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, { getPorts })(Ports);
+
+
+
+// To filter only Canada Ports
+// {
+//   const canada = this.props.ports.filter(port => port.border === "Canadian Border");
+//   canada.map(c => 
+//     <PortCard 
+//       key={c.id}
+//       port={c}
+// )}
+
+
+// {
+//   this.props.ports.map(port => 
+//   <PortCard 
+//     key={port.id} 
+//     port={ port } />
+//   )
+// }
