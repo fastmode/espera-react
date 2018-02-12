40597ccf24ebfd9e92a28403dcb877ecbf01d18d
Art Rodriguez <art.rodriguez@live.com>
2018-02-11 21:50:22 -0500

    add font awesome and heart icon to toggle Favorite port

--- client/src/components/FavoritePortCard.js
+++ client/src/components/FavoritePortCard.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 
 const FavoritePortCard = ({ port, onPortClick, index }) => (
-  <div key={port.id} className="PortCard" onClick={() => onPortClick(index)} >
+  <div key={port.id} className="PortCard"  >
     <h3>{port.port_name}</h3>
     <p>{port.border}</p>
     <p>
       {port.crossing_name} <br />
-      Hours: {port.hours}
+      Hours: {port.hours} <br />
+      <i class="fa fa-heart" onClick={() => onPortClick(index)}></i>
     </p>
   </div>
 )
--- client/src/components/PortCard.js
+++ client/src/components/PortCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const PortCard = ({ port, onPortClick }) => (
-  <div key={port.id} className="PortCard" onClick={() => onPortClick(port)} >
+  <div key={port.id} className="PortCard"  >
     <h3>{port.port_name}</h3>
     <p>
       {port.crossing_name} <br />
-      Hours: {port.hours}
+      Hours: {port.hours} <br />
+      <i class="fa fa-heart-o" onClick={() => onPortClick(port)}></i>
     </p>
   </div>
 )
