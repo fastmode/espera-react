67cd2355e9713207d0e49477b774648983ef2414
Art Rodriguez <art.rodriguez@live.com>
2018-04-14 16:20:55 -0400

    deleted store.js and update index.js imports

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-// import store from './store.js';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import ports from './reducers/ports';
-
 import './index.css';
 
 const reducers = combineReducers({ ports });
-
-// const middleware = [thunk];
 
 const store = createStore(
   reducers,
