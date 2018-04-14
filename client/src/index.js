bbb21b9b210890683062eb5ea9e8eb1812c79451
Art Rodriguez <art.rodriguez@live.com>
2018-04-14 16:19:06 -0400

    moved code from store.js over to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import store from './store.js';
+// import store from './store.js';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import ports from './reducers/ports';
 
 import './index.css';
+
+const reducers = combineReducers({ ports });
+
+// const middleware = [thunk];
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+)
 
 ReactDOM.render(
   <Provider store={store}>
--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import ports from './reducers/ports';
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import thunk from 'redux-thunk';
+// import ports from './reducers/ports';
 
-const reducers = combineReducers({ ports });
+// const reducers = combineReducers({ ports });
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
-export default createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(...middleware)
-)
+// export default createStore(
+//   reducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//   applyMiddleware(...middleware)
+// )
